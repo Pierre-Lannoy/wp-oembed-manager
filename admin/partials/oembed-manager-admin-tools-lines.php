05fd67d894444cbf32a43f2919d13cd109b1a866
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-03-18 15:27:59 +0100

    Changes text-domain to the right value.

--- admin/partials/oembed-manager-admin-tools-lines.php
+++ admin/partials/oembed-manager-admin-tools-lines.php
@@ -14,16 +14,16 @@ use Oemm\System\Environment;
 ?>
 
 <div class="alignleft actions bulkactions">
-    <label for="limit-selector" class="screen-reader-text"><?php esc_html_e('Number of posts to display', 'opcache-manager');?></label>
+    <label for="limit-selector" class="screen-reader-text"><?php esc_html_e('Number of posts to display', 'oembed-manager');?></label>
     <select name="limit-<?php echo $which; ?>" id="limit-selector-<?php echo $which; ?>">
 		<?php foreach ($list->get_line_number_select() as $line) { ?>
             <option <?php echo $line['selected']; ?>value="<?php echo $line['value']; ?>"><?php echo $line['text']; ?></option>
 		<?php } ?>
     </select>
-    <input type="submit" name="dolimit-<?php echo $which; ?>" id="dolimit-<?php echo $which; ?>" class="button action" value="<?php esc_html_e('Apply', 'opcache-manager');?>"  />
+    <input type="submit" name="dolimit-<?php echo $which; ?>" id="dolimit-<?php echo $which; ?>" class="button action" value="<?php esc_html_e('Apply', 'oembed-manager');?>"  />
 </div>
 
 <div class="alignleft actions bulkactions">
-    <input style="margin-left:10px;" type="submit" name="doinvalidate-<?php echo $which; ?>"  id="doinvalidate-<?php echo $which; ?>" class="button-primary action" value="<?php esc_html_e('Clear All Caches', 'opcache-manager');?>"  />
-    <input style="margin-left:10px;" type="submit" name="dowarmup-<?php echo $which; ?>" id="dowarmup-<?php echo $which; ?>" class="button-primary action" value="<?php echo esc_html__( 'Update All Caches', 'opcache-manager' );?>"  />
+    <input style="margin-left:10px;" type="submit" name="doinvalidate-<?php echo $which; ?>"  id="doinvalidate-<?php echo $which; ?>" class="button-primary action" value="<?php esc_html_e('Clear All Caches', 'oembed-manager');?>"  />
+    <input style="margin-left:10px;" type="submit" name="dowarmup-<?php echo $which; ?>" id="dowarmup-<?php echo $which; ?>" class="button-primary action" value="<?php echo esc_html__( 'Update All Caches', 'oembed-manager' );?>"  />
 </div>
