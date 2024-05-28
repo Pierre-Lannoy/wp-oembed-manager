fdad0b90c2b46a7beacc980f71e04344376dea9a
Pierre Lannoy <pierre@hosterra.eu>
2024-05-28 14:06:23 +0200

    Ready to release 3.0.0 on wp.org

--- oembed-manager.php
+++ oembed-manager.php
@@ -10,7 +10,7 @@
  * Plugin Name:       oEmbed Manager
  * Plugin URI:        https://perfops.one/oembed-manager
  * Description:       Manage oEmbed capabilities of your website and take a new step in the GDPR compliance of your embedded content.
- * Version:           2.14.0
+ * Version:           3.0.0
  * Requires at least: 6.2
  * Requires PHP:      8.1
  * Author:            Pierre Lannoy / PerfOps One
